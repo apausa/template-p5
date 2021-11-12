457060419d312395205d6c1285fc4578c04462c2
pabloapausa <pabloapausa@outlook.com>
2021-11-12 22:19:16 +0100

    Follow 'Getting Started'

--- p5/src/21/11/12/sketch.js
+++ p5/src/21/11/12/sketch.js
@@ -3,6 +3,10 @@ function setup() {
 }
 
 function draw() {
-  background(220, 0, 220);
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
